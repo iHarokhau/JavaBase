6e1ccafdee019d70147131ca802277bda005fe2a
Ihar Harokhau <harokhaui@gmail.com>
2025-01-06 22:17:33 +0300

    Added completed first String's homework

--- Lesson1/src/main/java/Homework/ReverseTheString.java
+++ Lesson1/src/main/java/Homework/ReverseTheString.java
@@ -1,4 +1,28 @@
 package Homework;
 
+/*
+Перевернуть строку и вывести на консоль
+String string = "I love Java"
+ */
+
 public class ReverseTheString {
+    /**
+     * Метод main используется для хранения начальных данных,
+     * запуска программы и вывода в консоль наших результатов.
+     * @param args
+     */
+    public static void main(String[] args) {
+        String string = "I love Java";
+
+        System.out.println(string + "\n" + reverseString(string));
+    }
+
+    /**
+     * Метод reverseString реверсирует строку и возвращает результат.
+     * @param string
+     * @return
+     */
+    private static String reverseString(String string){
+        return new StringBuilder(string).reverse().toString();
+    }
 }
