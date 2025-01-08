8c74a76bc6ab6592f4d0a3842407a910f689a746
Ihar Harokhau <harokhaui@gmail.com>
2025-01-08 20:04:25 +0300

    Added Class "IsStringPalindrome" homework.

---
+++ Lesson1/src/main/java/Homework/Palindrome/IsStringPalindrome.java
@@ -0,0 +1,14 @@
+package Homework.Palindrome;
+
+/*
+ Определить, что строка является палиндромом
+ Сложность по памяти O(1), не создавать новые String, StringBuilder
+ Примеры:
+ abc - false
+ 112233 - false
+ aba - true
+ 112211 - true
+*/
+
+public class IsStringPalindrome {
+}
