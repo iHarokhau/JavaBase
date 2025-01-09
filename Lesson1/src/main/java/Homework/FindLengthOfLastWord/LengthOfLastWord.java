865e67aeabf9ec2e92d275501b8904b67758dbcd
Ihar Harokhau <harokhaui@gmail.com>
2025-01-09 14:50:14 +0300

    Added ready "IsStringPalindrome" homework.

--- Lesson1/src/main/java/Homework/FindLengthOfLastWord/LengthOfLastWord.java
+++ Lesson1/src/main/java/Homework/FindLengthOfLastWord/LengthOfLastWord.java
@@ -1,34 +1,47 @@
 package Homework.FindLengthOfLastWord;
 
+import java.util.Scanner;
+
 /*
  Найти длину последнего слова в строке. В строке только буквы и пробелы.
  "Hello world" - 5
  "    fly me    to the moon    " - 4
 */
 
-import java.util.Scanner;
-
 public class LengthOfLastWord {
+    /**
+     * Метод "main" используется для запуска программы.
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println(lengthOfLastWord(textInput()));
     }
 
+    /**
+     * Метод "textInput" используется для ввода текста с клавиатуры в консоль
+     * и его хранения в переменной "scanner".
+     * @return
+     */
     private static String textInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter text: ");
         return scanner.nextLine();
     }
 
+    /**
+     * Метод "lengthOfLastWord" вычиляет длину последнего слова в строке.
+     * @param text
+     * @return
+     */
     private static Integer lengthOfLastWord(String text) {
-        int i;
-        for (i = text.length() - 1; i >= 0; i--) {
+        text = text.trim();
+        int spaceIndex = -1;
+
+        for (int i = 0; i < text.length(); i++) {
             if (text.charAt(i) == ' ') {
-                return i = text.length() - i - 1;
-            }
-            if (i == -1){
-                i = text.length();
+                spaceIndex = i;
             }
         }
-        return text.length();
+        return text.length() - spaceIndex - 1;
     }
 }
