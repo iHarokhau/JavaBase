a2e169d270d7c8eaf2322d0836d054608609b553
Ihar Harokhau <harokhaui@gmail.com>
2025-01-09 18:28:35 +0300

    Added ready "IsStringPalindrome" homework.

--- Lesson1/src/main/java/Homework/Palindrome/IsStringPalindrome.java
+++ Lesson1/src/main/java/Homework/Palindrome/IsStringPalindrome.java
@@ -1,5 +1,7 @@
 package Homework.Palindrome;
 
+import java.util.Scanner;
+
 /*
  Определить, что строка является палиндромом
  Сложность по памяти O(1), не создавать новые String, StringBuilder
@@ -11,4 +13,46 @@ package Homework.Palindrome;
 */
 
 public class IsStringPalindrome {
+
+    /**
+     * Method "main" runs the program and output the result to the console.
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (!isPalindrome(inputValue())){
+            System.out.println("The string isn't palindrome!");
+        } else{
+            System.out.println("The string is palindrome!");
+        }
+    }
+
+    /**
+     * Method "inputValue" reads the value from the keyboard
+     * and saves it to the variable "scanner".
+     * @return
+     */
+    private static String inputValue (){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a value: ");
+        return scanner.nextLine();
+    }
+
+    /**
+     * Method "isPalindrome" divides the length of the string in half
+     * and calculates matching values on the left and right.
+     * @param value
+     * @return
+     */
+    private static boolean isPalindrome(String value){
+        boolean result = false;
+
+        for (int i = 0; i < (value.length() / 2); i++) {
+            if (value.charAt(i) != value.charAt(value.length() - i - 1)){
+                result = false;
+            }
+            else result = true;
+        }
+
+        return result;
+    }
 }
