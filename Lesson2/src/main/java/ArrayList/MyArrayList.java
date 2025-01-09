75ae5d8a212eb169507bcb01cd8c75df5d7d0965
Ihar Harokhau <harokhaui@gmail.com>
2025-01-09 19:11:34 +0300

    Created Class "MyArrayList".

---
+++ Lesson2/src/main/java/ArrayList/MyArrayList.java
@@ -0,0 +1,28 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+/*
+Реализовать MyArrayList.
+ Методы, обязательные к реализации:
+- добавить элемент: add(T element),
+- добавить элемент по индексу: add(int index, T element),
+- получить элемент: get(int index),
+- удалить элемент remove(int index),
+- очистить всю коллекцию: clear(),
+- отсортировать коллекцию: sort(Comparator<T> comparator)
+ */
+
+
+
+public class MyArrayList {
+    private String name;
+
+    private MyArrayList(String name){
+        this.name = name;
+    }
+
+    private static ArrayList createArrayList(){
+
+    }
+}
