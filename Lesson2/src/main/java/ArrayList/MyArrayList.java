eab4fa5c03ab13160959b33a601a4772ac7baed5
Ihar Harokhau <harokhaui@gmail.com>
2025-01-10 18:45:59 +0300

    Added Class "MyArrayList".

--- Lesson2/src/main/java/ArrayList/MyArrayList.java
+++ Lesson2/src/main/java/ArrayList/MyArrayList.java
@@ -1,6 +1,7 @@
 package ArrayList;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /*
 Реализовать MyArrayList.
@@ -29,12 +30,33 @@ public class MyArrayList {
         name.add("Grigory");
         name.add("Vladislav");
         name.add("Katrin");
-
-        System.out.println(name);
-
-        name.add(3, "Vladimir");
-
         System.out.println(name);
     }
 
+    private ArrayList<String> addElement(ArrayList<String> name) {
+        name.add("Ihar");
+        System.out.println(name);
+        return name;
+    }
+
+    private ArrayList<String> addElementWithIndex(int index, ArrayList<String> name) {
+        name.add(3, "Vladimir");
+        System.out.println(name);
+        return name;
+    }
+
+    private ArrayList<String>
+        System.out.println(name.get(6));
+
+        name.remove(4);
+        System.out.println(name);
+
+        name.clear();
+        System.out.println(name);
+
+        name.sort(Comparator.naturalOrder());
+        System.out.println(name);
+
+        name.sort(Comparator.reverseOrder());
+        System.out.println(name);
 }
