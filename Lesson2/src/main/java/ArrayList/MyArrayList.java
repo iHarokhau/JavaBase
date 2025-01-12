e21919df4c5cb0fb2ac8b9648a7697245fdf058f
Ihar Harokhau <harokhaui@gmail.com>
2025-01-12 23:58:56 +0300

    Add ready methods "My Array List" homework.

--- Lesson2/src/main/java/ArrayList/MyArrayList.java
+++ Lesson2/src/main/java/ArrayList/MyArrayList.java
@@ -31,32 +31,47 @@ public class MyArrayList {
         name.add("Vladislav");
         name.add("Katrin");
         System.out.println(name);
+
+        System.out.println(addElement(name, "Ihar"));
+        System.out.println(addElementWithIndex(3, "Vladimir", name));
+        getElementNumber(name, 5);
+        removeElement(name,2);
+        naturalSortMyArray(name);
+        reverseSortMyArray(name);
+        clearMyArray(name);
     }
 
-    private ArrayList<String> addElement(ArrayList<String> name) {
-        name.add("Ihar");
-        System.out.println(name);
+    public static ArrayList<String> addElement(ArrayList<String> name, String anyName) {
+        name.add(anyName);
         return name;
     }
 
-    private ArrayList<String> addElementWithIndex(int index, ArrayList<String> name) {
-        name.add(3, "Vladimir");
-        System.out.println(name);
+    private static ArrayList<String> addElementWithIndex(int index, String anyName, ArrayList<String> name) {
+        name.add(index, anyName);
         return name;
     }
 
-    private ArrayList<String>
-        System.out.println(name.get(6));
+    private static void getElementNumber(ArrayList<String> name, int number) {
+        System.out.println(name.get(number));
+    }
 
-        name.remove(4);
+    private static void removeElement(ArrayList<String> name, int index) {
+        name.remove(index);
         System.out.println(name);
+    }
 
+    private static void clearMyArray(ArrayList<String> name) {
         name.clear();
         System.out.println(name);
+    }
 
+    private static void naturalSortMyArray(ArrayList<String> name) {
         name.sort(Comparator.naturalOrder());
         System.out.println(name);
+    }
 
+    private static void reverseSortMyArray(ArrayList<String> name) {
         name.sort(Comparator.reverseOrder());
         System.out.println(name);
+    }
 }
